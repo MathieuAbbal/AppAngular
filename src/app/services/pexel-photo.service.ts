58e009fa57d22a90af2ec6a488d7681c9809c8ae
mathieu abbal <mathieuabbal@hotmail.com>
2021-09-30 11:44:56 +0200

    proxy

--- src/app/services/pexel-photo.service.ts
+++ src/app/services/pexel-photo.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient,  HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import {catchError} from 'rxjs/operators'
+import { catchError } from 'rxjs/operators';
 
-const httpOptions ={
+const httpOptions = {
   headers: new HttpHeaders({
-   'Access-Control-Allow-Origin': '*',
-   'Authorization': '563492ad6f91700001000001acabc9f13a564f05a35627896b1a1bfd'
-  })
-
-
-}
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods':
+      'GET, PUT, POST, DELETE, HEAD, OPTIONS, PATCH, PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK',
+    'Access-Control-Expose-Headers': ' *',
+    Authorization: '563492ad6f91700001000001acabc9f13a564f05a35627896b1a1bfd',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
@@ -19,17 +21,16 @@ export class PexelPhotoService {
   constructor(private http: HttpClient) {}
 
   getData(search: String, per_page: Number): Observable<any> {
+    console.log(search);
+    console.log(per_page);
 
-    console.log(search)
-    console.log(per_page)
+    const uri ='/search?query='+search+'&per_page='+per_page;
 
-    const url = 'https://api.pexels.com/v1/search?query='+search+'&per_page='+per_page;
-
-    return this.http.get<any>(url,httpOptions)
-                    .pipe(catchError(this.handleError))
+    return this.http
+              .get<any>(uri, httpOptions)
+              .pipe(catchError(this.handleError));
   }
-  handleError(error: { message: any; }){
-    return throwError (error.message || "server erreur")
-
+  handleError(error: { message: any }) {
+    return throwError(error.message || 'server erreur');
   }
 }
